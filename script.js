23c90b13fbe636978cefc41c110334e877f0493a
DachepallySahithi <130071155+DachepallySahithi@users.noreply.github.com>
2025-01-31 18:18:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+function getNumbers() {
+	return new Promise((resolve) => {
+		setTimeOut(() => resolve([1,2,3,4]), 3000);
+	});
+}
+
+getNumbers()
+.then((numbers) =>{
+	return new Promise((resolve) =>{
+		setTimeOut(()=>{
+			const evens = numbers.filter(num => num % 2 === 0);
+			document.getElementById("output").innerText = evens.joins(", ");
+			resolve(evens);
+		}, 1000);
+	});
+})
+.then((evens) => {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+        const doubled = evens.map(num => num * 2);
+        document.getElementById("output").innerText = doubled.join(", ");
+        resolve(doubled);
+		},2000);
+	});
+})
+.catch((error) => {
+    document.getElementById("output").innerText = "Error: " + error;
+  });
+})
